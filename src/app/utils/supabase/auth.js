9170e794644c94101f2228ee53ab518a7afbf223
iLuvMelon <dylankane0117@gmail.com>
2026-03-29 17:46:40 -0400

    added auth logic

---
+++ src/app/utils/supabase/auth.js
@@ -0,0 +1,33 @@
+//NOTE: Need to implement error handling
+import { createClient } from "server.js";
+
+export async function signUp(email, password) {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: "https://example.com/welcome",
+    },
+  });
+}
+
+export async function signIn(email, password) {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+}
+
+export async function resetPass(email) {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "http://example.com/account/update-password",
+  });
+}
+
+export async function updatePass(password) {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.updateUser({ password });
+}
